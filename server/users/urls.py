eed31d7c38c757cfd28223ab6e65b6df5178bf1b
Aditya-Prakash14 <ap312038@gmail.com>
2026-01-12 23:31:58 +0530

    new serializers and views

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from .views import UserProfileView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserProfileView, UserRegistrationView,
+    UserViewSet, LeaderboardView
+)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('user/profile/', UserProfileView.as_view(), name='api-user-profile'),
+    path('auth/register/', UserRegistrationView.as_view(), name='api-register'),
+    path('auth/profile/', UserProfileView.as_view(), name='api-profile'),
+    path('leaderboard/', LeaderboardView.as_view(), name='api-leaderboard'),
+    path('', include(router.urls)),
 ]
